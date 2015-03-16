07b5b4d7e4712c157ce546eae7c15c8f7b3085ab
Christian Niedermayer <sdchrisn@gmail.com>
2015-03-15 22:18:38 -0700

    Setup code to handle redis data, need to setup client.select()

--- app.js
+++ app.js
@@ -4,44 +4,61 @@ var app = express();
 var bodyParser = require('body-parser');
 var urlencode = bodyParser.urlencoded({ extended: false });
 
+// middleware: folder of our static files
+app.use(express.static('public'));
+
+var redis = require('redis');
+var client = redis.createClient();
+
+client.hset('comments', 'One', 'body stuff');
+client.hset('comments', 'Two', 'body stuff');
+client.hset('comments', 'Three', 'body stuff');
+
    // -- refactored to middleware --
-   //
    //app.get('/', function(req, res){
    //    res.send('Hello World!\n');
    //});
 
-// middleware: folder of our static files
-app.use(express.static('public'));
-
-//var comments = [
-//   {title: 'One', body: 'talking all over the place'},
-//   {title: 'Two', body: 'talking all over the place'},
-//   {title: 'Three', body: 'talking all over the place'}
-//];
-
-var comments = {
-   'One': 'talking all over the place',
-   'Two': 'here we go again',
-   'Three': 'A third time we speak'
-};
-
 var commentPath = '/comments';
 
 app.get(commentPath , function(request, response){
-   response.json(comments);
+   client.hkeys('comments', function(error, titles) {
+      if(error) throw error;
+
+      response.json(titles);
+   });
 });
 app.post(commentPath , urlencode, function(request, response){
    var newComment = request.body;
-   var obj = {};
-   comments[newComment.title] = newComment.body;
-   obj[newComment.title] = newComment.body;
-   response.status(201).json(obj);
+   client.hset('comments', newComment.title, newComment.body, function(error) {
+      if(error) throw error;
+
+      var obj = {};
+      obj[newComment.title] = newComment.body;
+      response.status(201).json(obj);
+   });
 });
-
-
 
 
 // This line will bind our app to the network
 //app.listen(3000);
 
 module.exports = app;
+
+// GET & POST - pre redis
+//var comments = {
+//   'One': 'talking all over the place',
+//   'Two': 'here we go again',
+//   'Three': 'A third time we speak'
+//};
+//app.get(commentPath , function(request, response){
+//   response.json(comments);
+//});
+//app.post(commentPath , urlencode, function(request, response){
+//   var newComment = request.body;
+//   var obj = {};
+//   comments[newComment.title] = newComment.body;
+//   obj[newComment.title] = newComment.body;
+//   response.status(201).json(obj);
+//});
+
